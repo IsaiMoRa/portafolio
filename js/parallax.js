e95dd754241932593854c98a0289e2162267e9c1
Josue Isai Morales <isai.morales1307@gmail.com>
2025-01-04 09:00:44 -0600

    se creo cover del portafolio

---
+++ js/parallax.js
@@ -0,0 +1,98 @@
+// ------- Osmo [https://osmo.supply/] ------- //
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Register GSAP Plugins
+    gsap.registerPlugin(ScrollTrigger);
+    // Parallax Layers
+    document
+      .querySelectorAll("[data-parallax-layers]")
+      .forEach((triggerElement) => {
+        let tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: triggerElement,
+            start: "0% 0%",
+            end: "100% 0%",
+            scrub: 0
+          }
+        });
+        const layers = [
+          { layer: "1", yPercent: 70 },
+          { layer: "2", yPercent: 55 },
+          { layer: "3", yPercent: 40 },
+          { layer: "4", yPercent: 10 }
+        ];
+        layers.forEach((layerObj, idx) => {
+          tl.to(
+            triggerElement.querySelectorAll(
+              `[data-parallax-layer="${layerObj.layer}"]`
+            ),
+            {
+              yPercent: layerObj.yPercent,
+              ease: "none"
+            },
+            idx === 0 ? undefined : "<"
+          );
+        });
+      });
+  });
+  /* Lenis */
+  const lenis = new Lenis();
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+  gsap.ticker.lagSmoothing(0);
+  
+
+
+    /*Start | Text Rotating*/
+    var words = document.getElementsByClassName("word");
+    var wordArray = [];
+    var currentWord = 0;
+
+    words[currentWord].style.opacity = 1;
+    for (var i = 0; i < words.length; i++) {
+      splitLetters(words[i]);
+    }
+    function changeWord() {
+      var cw = wordArray[currentWord];
+      var nw =
+        currentWord == words.length - 1
+          ? wordArray[0]
+          : wordArray[currentWord + 1];
+      for (var i = 0; i < cw.length; i++) {
+        animateLetterOut(cw, i);
+      }
+      for (var i = 0; i < nw.length; i++) {
+        nw[i].className = "letter behind";
+        nw[0].parentElement.style.opacity = 1;
+        animateLetterIn(nw, i);
+      }
+      currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
+    }
+    function animateLetterOut(cw, i) {
+      setTimeout(function () {
+        cw[i].className = "letter out";
+      }, i * 80);
+    }
+    function animateLetterIn(nw, i) {
+      setTimeout(function () {
+        nw[i].className = "letter in";
+      }, 340 + i * 80);
+    }
+    function splitLetters(word) {
+      var content = word.innerHTML;
+      word.innerHTML = "";
+      var letters = [];
+      for (var i = 0; i < content.length; i++) {
+        var letter = document.createElement("span");
+        letter.className = "letter";
+        letter.innerHTML = content.charAt(i).replace(/ /g, "");
+        word.appendChild(letter);
+        letters.push(letter);
+      }
+      wordArray.push(letters);
+    }
+    changeWord();
+    setInterval(changeWord, 6000);
+    /*End | Text Rotating*/
